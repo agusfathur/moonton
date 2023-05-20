deb861afd25d5b6eeee709184576b715ddeafdac
Moh Agus Fathur Rozi <gusfathur25@gmail.com>
2023-05-20 13:51:07 +0700

    feat : slicing subscription plan page

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,9 @@ Route::prefix('prototype')->name('prototype.')->group(function () {
     route::get('/dashboard', function () {
         return Inertia::render('Prototype/Dashboard');
     })->name('dashboard');
+    route::get('/subscriptionPlan', function () {
+        return Inertia::render('Prototype/SubscriptionPlan');
+    })->name('subscriptionPlan');
 });
 
 require __DIR__ . '/auth.php';
